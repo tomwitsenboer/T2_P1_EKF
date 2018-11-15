e92e8decde16201b8e2c247e952735e3a9759ca8
tomwitsenboer <40236346+tomwitsenboer@users.noreply.github.com>
2018-11-15 12:02:18 +0100

    Update FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -69,9 +69,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       //Convert radar from polar to cartesian coordinates and initialize state (x, y, dx, dy)
-	  float rho = measurement_pack.raw_measurements_[0]; 
-	  float phi = measurement_pack.raw_measurements_[1]; 
-	  float rho_dot = measurement_pack.raw_measurements_[2];
+	  double rho = measurement_pack.raw_measurements_[0]; 
+	  double phi = measurement_pack.raw_measurements_[1]; 
+	  double rho_dot = measurement_pack.raw_measurements_[2];
       ekf_.x_ << rho * cos(phi), rho * sin(phi), rho_dot * cos(phi), rho_dot * sin(phi);
     }
     
@@ -90,18 +90,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   ****************************************************************************/
   //from lecture 5-14
   //compute the time elapsed between the current and previous measurements
-  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  double dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
   previous_timestamp_ = measurement_pack.timestamp_;
-  float dt_2 = dt * dt;
-  float dt_3 = dt_2 * dt;
-  float dt_4 = dt_3 * dt;
+  double dt_2 = dt * dt;
+  double dt_3 = dt_2 * dt;
+  double dt_4 = dt_3 * dt;
   //Modify the F matrix so that the time is integrated
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
 
   //set the acceleration noise components (from lecture 5-14)
-  float noise_ax = 5;
-  float noise_ay = 5;
+  noise_ax = 5;
+  noise_ay = 5;
 
   //set the process covariance matrix Q
   ekf_.Q_ = MatrixXd(4, 4);

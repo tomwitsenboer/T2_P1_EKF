4e01e2b04b385fc590867af104be3b79284346c1
tomwitsenboer <40236346+tomwitsenboer@users.noreply.github.com>
2018-11-15 13:20:21 +0100

    Update FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -77,12 +77,12 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	  double rho = measurement_pack.raw_measurements_[0]; 
 	  double phi = measurement_pack.raw_measurements_[1]; 
 	  double rho_dot = measurement_pack.raw_measurements_[2];
-      ekf_.x_ << rho * cos(phi), rho * sin(phi), rho_dot * cos(phi), rho_dot * sin(phi);
+      ekf_.x_ << rho * cos(phi), rho * sin(phi), 0, 0;
     }
     
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
 	  //set the state with the initial location and zero velocity (from lecture 5-14)	
-      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0.0, 0.0;
     }
 
     // done initializing, no need to predict or update
